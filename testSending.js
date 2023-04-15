1c2844a50ac2cd977d9cd31b7274102855ea4cf3
Sen ZmaKi <phidelalvin@gmail.com>
2023-04-15 10:51:23 +0300

    feat - add - getTransactionDetails() retrieves transaction details specifc to an account address

--- testSending.js
+++ testSending.js
@@ -1,10 +1,10 @@
 const algosdk = require('algosdk');
-
-const algodToken = token={"x-api-key":'oMT938ZN9cacKTcIUlfQZ28sN6lyJCaS3lv2zYXF'}; // your API token
+const crypto = require('crypto')
+const algodToken = {"X-API-Key": 'oMT938ZN9cacKTcIUlfQZ28sN6lyJCaS3lv2zYXF'}; // your API token
 const algodServer = 'https://testnet-algorand.api.purestake.io/ps2'; // your Algod server address
 const algodPort = ''; // your Algod server port
 const client = new algosdk.Algodv2(algodToken, algodServer, algodPort);
-
+client.accountApplicationInformation
 let mnemonic = 'perfect faint onion powder citizen chuckle lesson life used donkey spike must tonight night visit voice floor fitness rare elevator twenty bracket corn about pool'
 let mainAccount = algosdk.mnemonicToSecretKey(mnemonic)
 console.log("Main account address: ", mainAccount.addr)
@@ -15,36 +15,49 @@ function generateAccount(count){
         accounts.push(algosdk.generateAccount())
         console.log("Account address: ", accounts[i].addr);
     }
-    return accounts
-
+    return accounts;
 }
 
-let sendAccounts = generateAccount(1);
-
-function sendVotes(mainAccount, sendAccounts){
-    for (sendAccount of sendAccounts){
-        ( async() => {
-            let params = await client.getTransactionParams().do();
-            let txn = {
-                "from": mainAccount.addr,
-                "to": sendAccount.addr,
-                "fee": 1,
-                "amount": 1000000, // 1 algo
-                "firstRound": params.firstRound,
-                "lastRound": params.lastRound,
-                "genesisID": params.genesisID,
-                "genesisHash": params.genesisHash,
-                "note": new Uint8Array(0)
-            }
-            let signedTxn = algosdk.signTransaction(txn, mainAccount.sk);
-            let sendTx = await client.sendRawTransaction(signedTxn.blob).do();
-        
-            console.log("Transaction: " + sendTx.txId);
-        })().catch( e => {
-            return console.log(e);
-        })
-
+async function hasVoted(mainAccount, voterId) {
+    const transactionNote = generateTransactionNote(voterId);
+    client.transactionB
+    const transactions = await client.accountInformation(mainAccount.addr).do();
+    console.log(transactions.transactions)
+    for (let txn of transactions["transactions"]) {
+        if (txn.txn.note === transactionNote) {
+            return true;
+        }
     }
+    return false;
 }
 
-sendVotes(mainAccount, sendAccounts)
+function generateTransactionNote(voterId) {
+    const hash = crypto.createHash('sha512');
+    hash.update(Buffer.from(voterId));
+    return hash.digest('base64');
+}
+
+async function sendVote(mainAccount, sendAccount, voterId){
+    if (!await hasVoted(mainAccount, voterId)){
+        console.log(voterId)
+        let params = await client.getTransactionParams().do();
+        let txn = {
+            "from": mainAccount.addr,
+            "to": sendAccount.addr,
+            "fee": 1,
+            "amount": 1000000, // 1 algo
+            "firstRound": params.firstRound,
+            "lastRound": params.lastRound,
+            "genesisID": params.genesisID,
+            "genesisHash": params.genesisHash,
+            "note": generateTransactionNote(voterId),
+        }
+        let signedTxn = algosdk.signTransaction(txn, mainAccount.sk);
+        let sendTx = await client.sendRawTransaction(signedTxn.blob).do();
+        console.log("Transaction: " + sendTx.txId);
+    }
+    else{return console.error(`Voter ${voterId} has already voted!!!`);}
+}
+
+let sendAccounts = generateAccount(1)[0];
+sendVote(mainAccount, sendAccounts, "123");
